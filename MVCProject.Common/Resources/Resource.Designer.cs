a453593d2fdec1495d6167aa5449ff14d953e413
Nidhichavan04 <nidhichavhan4@gmail.com>
2023-06-21 15:48:32 +0530

    resource

--- MVCProject.Common/Resources/Resource.Designer.cs
+++ MVCProject.Common/Resources/Resource.Designer.cs
@@ -1357,6 +1357,24 @@ namespace MVCProject.Common.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Month-Year.
+        /// </summary>
+        public static string Month_Year {
+            get {
+                return ResourceManager.GetString("Month_Year", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to MONTHLY EHS PERFORMANCE REVIEW REPORT.
+        /// </summary>
+        public static string MonthlyEHSPerformacereviewreport {
+            get {
+                return ResourceManager.GetString("MonthlyEHSPerformacereviewreport", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to My SOPs.
         /// </summary>
         public static string MySOP {
@@ -1546,6 +1564,15 @@ namespace MVCProject.Common.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Project.
+        /// </summary>
+        public static string Project {
+            get {
+                return ResourceManager.GetString("Project", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Quantity must be greater than zero..
         /// </summary>
         public static string QtyGreaterThanZeroRequired {
@@ -1677,6 +1704,15 @@ namespace MVCProject.Common.Resources {
         public static string ResetPasswordTokenIsExpired {
             get {
                 return ResourceManager.GetString("ResetPasswordTokenIsExpired", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Review Report.
+        /// </summary>
+        public static string ReviewReport {
+            get {
+                return ResourceManager.GetString("ReviewReport", resourceCulture);
             }
         }
         
@@ -2235,6 +2271,15 @@ namespace MVCProject.Common.Resources {
         public static string ValueIsRequired {
             get {
                 return ResourceManager.GetString("ValueIsRequired", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Vertical.
+        /// </summary>
+        public static string Vertical {
+            get {
+                return ResourceManager.GetString("Vertical", resourceCulture);
             }
         }
         
