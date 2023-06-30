bae7af919492ad9378c8d15c62cc9d3aa5e92530
Dp <dharmin0208@gmail.com>
2023-06-30 16:43:41 +0530

    Small Changes

--- MVCProject.Common/Resources/Resource.Designer.cs
+++ MVCProject.Common/Resources/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace MVCProject.Common.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -2442,6 +2442,15 @@ namespace MVCProject.Common.Resources {
         public static string Yesterday {
             get {
                 return ResourceManager.GetString("Yesterday", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Your Search.
+        /// </summary>
+        public static string YourSearch {
+            get {
+                return ResourceManager.GetString("YourSearch", resourceCulture);
             }
         }
     }
